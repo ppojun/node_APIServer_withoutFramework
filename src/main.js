b4d567daec439021f901fa93bb3c6baa494157e8
ppojun <120106@naver.com>
2022-02-14 18:10:46 +0900

    .

--- src/main.js
+++ src/main.js
@@ -13,5 +13,11 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
-  res.end('hello')
+  res.end('Hello!')
+})
+
+const PORT = 4000
+
+server.listen(PORT, () => {
+  console.log(`The Server is listening at port: ${PORT}`)
 })
