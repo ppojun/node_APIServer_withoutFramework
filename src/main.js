f1cd0ded756c049c3fd40bced0760f12e20a6a99
ppojun <120106@naver.com>
2022-02-15 18:18:22 +0900

    inMemoryPost

--- src/main.js
+++ src/main.js
@@ -12,6 +12,27 @@
 const http = require('http')
 
 /**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: 'my_first_post',
+    title: 'My first post',
+    content: 'Hello!',
+  },
+  {
+    id: 'my_second_post',
+    title: '나의 두번째 포스트',
+    content: 'second post!',
+  },
+]
+
+/**
  * Post
  *
  * GET /posts
@@ -19,23 +40,61 @@ const http = require('http')
  * POST /posts
  */
 const server = http.createServer((req, res) => {
+  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/
+  const postIdRegexResult =
+    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
+
   console.log(req.url)
   if (req.url === '/posts' && req.method === 'GET') {
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      totalCount: posts.length,
+    }
+
     res.statusCode = 200
-    res.end('List of posts')
-  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
-    res.statusCode = 200
-    res.end('Some content of post')
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.end(JSON.stringify(result))
+  } else if (postIdRegexResult && req.method === 'GET') {
+    // GET /posts/:id
+    const postId = postIdRegexResult[1]
+    const post = posts.find((_post) => _post.id === postId)
+
+    if (post) {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json; charset=utf-8')
+      res.end(JSON.stringify(post))
+    } else {
+      res.statusCode = 404
+      res.end('Post not found')
+    }
   } else if (req.url === '/posts' && req.method === 'POST') {
+    req.setEncoding('utf-8')
+    req.on('data', (data) => {
+      /**
+       * @typedef CreatePostBody
+       * @property {string} title
+       * @property {string} content
+       */
+
+      /** @type {CreatePostBody} */
+      const body = JSON.parse(data)
+      console.log(body)
+      posts.push({
+        id: body.title.toLowerCase().replace(/\s/g, '_'),
+        title: body.title,
+        content: body.content,
+      })
+    })
+
     res.statusCode = 200
     res.end('Creating post')
   } else {
     res.statusCode = 404
     res.end('Not found.')
   }
-
-  res.statusCode = 200
-  res.end('Hello!')
 })
 
 const PORT = 4000
