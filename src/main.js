332249cb6a4013161d9755efb05cf3354275d15b
ppojun <120106@naver.com>
2022-02-15 16:30:40 +0900

    .

--- src/main.js
+++ src/main.js
@@ -11,7 +11,29 @@
 
 const http = require('http')
 
+/**
+ * Post
+ *
+ * GET /posts
+ * GET /posts/:id
+ * POST /posts
+ */
 const server = http.createServer((req, res) => {
+  console.log(req.url)
+  if (req.url === '/posts' && req.method === 'GET') {
+    res.statusCode = 200
+    res.end('List of posts')
+  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]$/.test(req.url)) {
+    res.statusCode = 200
+    res.end('Some content of post')
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    res.statusCode = 200
+    res.end('Creating post')
+  } else {
+    res.statusCode = 404
+    res.end('Not found.')
+  }
+
   res.statusCode = 200
   res.end('Hello!')
 })
